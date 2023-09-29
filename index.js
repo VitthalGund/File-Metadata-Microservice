ebc41d463c91a741382f6620e7782a274452dcae
VitthalGund <vitthalgund30@gmail.com>
2023-09-29 18:28:48 +0530

    route for file analyses is added

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-var express = require('express');
-var cors = require('cors');
+let express = require('express');
+let cors = require('cors');
+const multer = require("multer");
 require('dotenv').config()
 
-var app = express();
+let app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
+let fileanalyse;
+// SET STORAGE
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    fileanalyse = file;
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.body.name)
+  }
+})
+
+let upload = multer({ storage: storage })
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('file'), async (req, res) => {
+  if (fileanalyse) {
+    res.status(200).json(fileanalyse);
+  }
 });
 
 
